b7ff2fad144c4a816806377adadc072cf8d15dc8
mac <rahulshres034@gmail.com>
2025-04-21 12:43:14 +0545

    Day 2 - View Js

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("homepage.ejs");
+});
 app.listen(3000, function () {
   console.log("Port Running");
 });
