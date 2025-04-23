1e746eb824711dd0ebb9e7b36c5c69706b58b355
mac <rahulshres034@gmail.com>
2025-04-23 16:53:34 +0545

    added ui of create blog

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
 app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.get("/", (req, res) => {
   res.render("homepage.ejs");
+});
+
+app.get("/createblog", (req, res) => {
+  res.render("createBlog");
+});
+
+app.post("/createBlog", async (req, res) => {
+  const { title, subtitle, description } = req.body;
+  console.log(title, subtitle, description);
 });
 app.listen(3000, function () {
   console.log("Port Running");
