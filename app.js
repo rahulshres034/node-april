6957731899e021e07b9b78a2f8c26ff8ae100f10
mac <rahulshres034@gmail.com>
2025-04-30 07:06:48 +0545

    update blog done

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { blog } = require("./model");
+const { where } = require("sequelize");
 const app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -25,6 +26,56 @@ app.post("/createBlog", async (req, res) => {
 
   console.log(createBlog);
   res.send("Blog Created");
+});
+
+app.get("/single/:id", async (req, res) => {
+  const id = req.params.id;
+  const singleBlog = await blog.findAll({
+    where: {
+      id: id,
+    },
+  });
+  console.log(singleBlog);
+
+  res.render("singleBlog", { blog: singleBlog });
+});
+
+app.get("/delete/:id", async (req, res) => {
+  const id = req.params.id;
+
+  const deleteBlog = await blog.destroy({
+    where: {
+      id: id,
+    },
+  });
+
+  res.redirect("/");
+});
+
+app.get("/editBlog/:id", async (req, res) => {
+  const id = req.params.id;
+  const singleBlog = await blog.findAll({
+    where: {
+      id: id,
+    },
+  });
+  console.log(singleBlog);
+
+  res.render("editBlog", { blog: singleBlog });
+});
+
+app.post("/editBlog/:id", async (req, res) => {
+  const id = req.params.id;
+  const { title, subtitle, description } = req.body;
+
+  try {
+    await blog.update({ title, subtitle, description }, { where: { id: id } });
+
+    res.redirect("/"); // or redirect to single/:id if you want
+  } catch (error) {
+    console.error(error);
+    res.send("Cannot edit blog");
+  }
 });
 
 app.listen(3000, function () {
